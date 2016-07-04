0a05a79b7d843b57b9fa2afc2a1702f93c166dc3
Juho Mäkinen <juho.makinen@applifier.com>
2016-07-04 11:14:33 +0300

    Revert "Add newer version on consumer.go"
    This reverts commit ae87adea0b7a2814915dfb231dd3111c5e224289.

--- consumer.go
+++ consumer.go
@@ -1,163 +1,193 @@
 package main
 
 import (
-    "strconv"
-    "strings"
-    "gopkg.in/Shopify/sarama.v1"
-    "errors"
-    "fmt"
+	//    "strconv"
+	"errors"
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/Shopify/sarama"
+	"github.com/bsm/sarama-cluster"
 )
 
 type Consumer struct {
-    config *sarama.Config
-    brokers []string
-    topic string
-    exitChannel chan bool
-    consumer sarama.Consumer
-    partitionConsumers []sarama.PartitionConsumer
+	groupID      string
+	seed_brokers []string
+	topic_prefix string
+	exitChannel  chan bool
 
+	client *cluster.Client
+
+	KafkaStatus KafkaStatus
+	config      *cluster.Config
+
+	consumers      map[string]*cluster.Consumer
+	consumersMutex sync.Mutex
 }
 
-func (s *Consumer) Start(brokers []string, topic string, partition string, channel chan Message) {
+type KafkaStatus struct {
+	Brokers []string
 
-    var initialOffset  int64
-    var err error
-    var partitions []int32
-
-    s.exitChannel = make(chan bool)
-    s.brokers = brokers
-    s.topic = topic
-    initialOffset = sarama.OffsetNewest
-
-
-    s.config = sarama.NewConfig()
-    s.config.ClientID = "kafkalogs"
-
-    all_brokers, all_partitions, err := s.fetchMetadata(s.config)
-    if err != nil {
-        logger.Fatalf("error fetching metadata from broker: %v", err)
-    }
-
-
-    if s.consumer, err = sarama.NewConsumer(all_brokers, s.config); err != nil {
-        logger.Fatalln(err)
-    }
-
-    if partition != "" {
-        partitions = []int32{}
-        for _, v := range strings.Split(partition, ",") {
-            if i, err := strconv.Atoi(v); err == nil {
-                partitions = append(partitions, int32(i))
-            }
-        }
-    } else {
-        partitions = all_partitions
-    }
-
-    if len(partitions) == 0 {
-        logger.Fatalln("No partitions listed")
-    } else {
-        logger.Printf("Consuming topic %s for partitions: %v\n", topic, partitions)
-    }
-
-    for _, partition := range partitions {
-        pc := ConsumePartition(s.consumer, topic, partition, initialOffset, channel)
-        s.partitionConsumers = append(s.partitionConsumers, pc)
-    }
-
+	Topics []string
+	// Map: Topic name -> []int32 slice of partitions
+	TopicPartitions map[string][]int32
 }
 
-// WaitForExit waits for os signal, then closes the sarama connection.
-func (s *Consumer) Close() {
-    for _, pc := range s.partitionConsumers {
-        pc.AsyncClose()
-    }
+func NewKafkaStatus() KafkaStatus {
+	c := KafkaStatus{}
+	c.TopicPartitions = make(map[string][]int32)
 
-    s.consumer.Close();
+	return c
+}
+
+func NewConsumer(brokers []string, topic_prefix string) Consumer {
+
+	c := Consumer{}
+	c.seed_brokers = brokers
+	c.consumers = make(map[string]*cluster.Consumer)
+
+	c.groupID = "kafka2elasticsearch"
+
+	c.topic_prefix = topic_prefix
+
+	return c
+}
+
+func (s *Consumer) Init() error {
+
+	kafka_status, err := FetchKafkaMetadata(s.seed_brokers, s.topic_prefix)
+	if err != nil {
+		logger.Fatalf("error fetching metadata from broker: %v", err)
+	}
+	s.KafkaStatus = kafka_status
+
+	s.config = cluster.NewConfig()
+	s.config.Group.Return.Notifications = true
+
+	client, err := cluster.NewClient(s.KafkaStatus.Brokers, s.config)
+	if err != nil {
+		logger.Fatalf("error creating new kafka client: %v", err)
+		return err
+	}
+	s.client = client
+
+	return nil
+}
+
+func (s *Consumer) StartConsumingTopic(topic string) error {
+	consumer, err := cluster.NewConsumerFromClient(s.client, s.groupID, []string{topic})
+
+	if err != nil {
+		fmt.Printf("Error on StartConsumingTopic for topic %s: %+v\n", topic, err)
+		return err
+	}
+
+	s.consumersMutex.Lock()
+	s.consumers[topic] = consumer
+	defer s.consumersMutex.Unlock()
+
+	go func(notifications <-chan *cluster.Notification) {
+		for notification := range notifications {
+			fmt.Printf("Notification: %+v\n", notification)
+		}
+	}(consumer.Notifications())
+
+	go func(messages <-chan *sarama.ConsumerMessage) {
+		for message := range messages {
+			fmt.Printf("Message: %+v\n", message)
+		}
+	}(consumer.Messages())
+
+	fmt.Printf("Started consuming topic %s\n", topic)
+
+	return nil
 }
 
 func (s *Consumer) Wait() {
-    <-s.exitChannel
+	<-s.exitChannel
 }
 
 func ConsumePartition(consumer sarama.Consumer, topic string, partition int32, initialOffset int64, messages chan Message) sarama.PartitionConsumer {
-    var pc sarama.PartitionConsumer
-    pc, err := consumer.ConsumePartition(topic, partition, initialOffset)
-    if err != nil {
-        logger.Fatalln(err)
-    }
-    go func(pc sarama.PartitionConsumer, messages chan Message) {
-        for {
-            select {
-            case err := <-pc.Errors():
-                logger.Printf("Consumer error: %+v", err)
-            case msg := <-pc.Messages():
-                m := Message{}
-                m.Key = string(msg.Key)
-                m.Topic = msg.Topic
-                m.Partition = msg.Partition
-                m.Data = msg.Value
+	var pc sarama.PartitionConsumer
+	pc, err := consumer.ConsumePartition(topic, partition, initialOffset)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+	go func(pc sarama.PartitionConsumer, messages chan Message) {
+		for {
+			select {
+			case err := <-pc.Errors():
+				logger.Printf("Consumer error: %+v", err)
+			case msg := <-pc.Messages():
+				m := Message{}
+				m.Key = string(msg.Key)
+				m.Topic = msg.Topic
+				m.Partition = msg.Partition
+				m.Data = msg.Value
 
-                if err != nil {
-                    logger.Printf("Invalid JSON entry in log feed: %s\n", string(msg.Value))
-                    continue
-                }
+				if err != nil {
+					logger.Printf("Invalid JSON entry in log feed: %s\n", string(msg.Value))
+					continue
+				}
 
-                messages <- m
-            }
-        }
+				messages <- m
+			}
+		}
 
-    }(pc, messages)
+	}(pc, messages)
 
-    return pc
+	return pc
 }
 
 // connects to one of a list of brokers
-func (c *Consumer) connectToBroker(config *sarama.Config) (*sarama.Broker, error) {
-    var err error
-    for _, host := range c.brokers {
-        broker := sarama.NewBroker(host)
-        err = broker.Open(config)
-        if err != nil {
-            logger.Printf("error connecting to broker: %s %v", host, err)
-        } else {
-            return broker, nil
-        }
-    }
-    return nil, err
+func connectToBroker(config *sarama.Config, seed_brokers []string) (*sarama.Broker, error) {
+	var err error
+	for _, host := range seed_brokers {
+		broker := sarama.NewBroker(host)
+		err = broker.Open(config)
+		if err != nil {
+			logger.Printf("error connecting to broker: %s %v", host, err)
+		} else {
+			return broker, nil
+		}
+	}
+	return nil, err
 }
 
 // connects to the broker and fetches current brokers' address and partition ids
-func (c *Consumer) fetchMetadata(config *sarama.Config) ([]string, []int32, error) {
-    broker, err := c.connectToBroker(config)
-    if err != nil {
-        return nil, nil, err
-    }
-    request := sarama.MetadataRequest{Topics: []string{c.topic}}
-    response, err := broker.GetMetadata(&request)
-    if err != nil {
-        _ = broker.Close()
-        return nil, nil, err
-    }
+func FetchKafkaMetadata(seed_brokers []string, topic_search_prefix string) (KafkaStatus, error) {
+	kf := NewKafkaStatus()
 
-    if len(response.Brokers) == 0 {
-        return nil, nil, errors.New(fmt.Sprintf("Unable to find any broker for topic: %s", c.topic))
-    }
-    if len(response.Topics) != 1 {
-        return nil, nil, errors.New(fmt.Sprintf("Invalid number of topics: %d", len(response.Topics)))
-    }
+	config := sarama.NewConfig()
+	broker, err := connectToBroker(config, seed_brokers)
+	if err != nil {
+		return kf, err
+	}
+	request := sarama.MetadataRequest{}
+	response, err := broker.GetMetadata(&request)
+	if err != nil {
+		_ = broker.Close()
+		return kf, err
+	}
 
-    var brokers []string
-    for _, broker := range response.Brokers {
-        // log.Printf("broker: %q", broker.Addr())
-        brokers = append(brokers, broker.Addr())
-    }
+	if len(response.Brokers) == 0 {
+		return kf, errors.New(fmt.Sprintf("Unable to find any brokers"))
+	}
 
-    var partitions []int32
-    for _, partition := range response.Topics[0].Partitions {
-        //logger.Printf("partition: %v, leader: %v", partition.ID, partition.Leader)
-        partitions = append(partitions, partition.ID)
-    }
+	for _, broker := range response.Brokers {
+		// log.Printf("broker: %q", broker.Addr())
+		kf.Brokers = append(kf.Brokers, broker.Addr())
+	}
+	fmt.Printf("Brokers: %+v\n", kf.Brokers)
 
-    return brokers, partitions, broker.Close()
+	for _, topic := range response.Topics {
+		if strings.HasPrefix(topic.Name, topic_search_prefix) {
+			kf.Topics = append(kf.Topics, topic.Name)
+			for _, partition := range topic.Partitions {
+				kf.TopicPartitions[topic.Name] = append(kf.TopicPartitions[topic.Name], partition.ID)
+			}
+		}
+	}
+	return kf, broker.Close()
 }
